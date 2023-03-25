6efb67a0c0b86c0ad881b36958731204f67efadd
123Chaos <799510721@qq.com>
2023-03-26 01:16:45 +0800

    update

---
+++ throttle.js
@@ -0,0 +1,12 @@
+// 节流
+function throttled(fn, delay = 500) {
+  let timer = null;
+  return function (...args) {
+    if (!timer) {
+      timer = setTimeout(() => {
+        fn.apply(this, args);
+        timer = null;
+      }, delay);
+    }
+  };
+}
